7ab0e1b328652458018e410f8beb863481a890cb
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-12-15 11:59:45 -0700

    created post link

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var path = require("path"); 
+var path = require("path");
 
 var app = express();
 var PORT = 3000;
@@ -9,19 +9,42 @@ app.use(express.json());
 
 var tables = [
     {
-    Name: "person",
-    Phone: 123,
-    Email: 123,
-    ID: 123
+        Name: "person",
+        Phone: 123,
+        Email: 123,
+        ID: 123
     }
-  ];
+];
 
- app.get("/api/tables", function(req, res) {
+var waitlist = [
+    {
+        Name: "person2",
+        Phone: 123,
+        Email: 123,
+        ID: 123
+    }
+];
+
+app.get("/api/tables", function (req, res) {
     return res.json(tables);
 });
 
+app.get("/api/waitlist", function (req, res) {
+    return res.json(waitlist);
+});
 
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+// Create New Characters - takes in JSON input
+app.post("/api/tables", function(req, res) {
+
+    var newTable = req.body;
+  
+    tables.push(newTable);
+  
+    res.json(newTable);
   });
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
